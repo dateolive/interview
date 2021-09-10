18a66a8d14bda35a20b3108df97b54b79feda38d
dateolive <2448282543@qq.com>
2021-09-10 20:49:26 +0800

    2021/9/10 冲冲冲！！实习

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -4,48 +4,42 @@ module.exports = [
     text: '语言',
     link: '/code/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
     items: [
-      { text: 'Java', link: '/pages/55bf8c/' },
-      { text: 'C/C++', link: '/pages/7426b1/' }
+      { text: 'Java', link: '/pages/6824ac/' },
+      { text: 'C/C++', link: '/pages/28591f/' }
     ],
   },
   {
     text: '数据库',
     link: '/database/',
     items: [
-      { text: 'MySQL', link: '/pages/1aef7c/' },
+      { text: 'MySQL', link: '/pages/5bf967/' },
       { text: 'Redis', link: '/pages/b9a12d/' },
-      { text: 'MongoDB', link: '/pages/9fbb16/' },
+      { text: 'MongoDB', link: '/pages/c4ff75/' },
     ],
   },
   {
     text: '学科',
     link: '/base/',
     items: [
-      { text: '操作系统', link: '/pages/5bfa72/' },
-      { text: '计算机网络', link: '/pages/6bb966/' },
+      { text: '操作系统', link: '/pages/ff28d3/' },
+      { text: '计算机网络', link: '/pages/2ea435/' },
     ],
   },
   {
     text: '框架',
     link: '/frame/',
     items: [
-      { text: 'spring全家桶', link: '/pages/9baa5c/', 
-        items:[
-          { text: 'Spring', link: '/pages/eaabb9/' },
-          { text: 'SpringMVC', link: '/pages/eaabb9/' },
-          { text: 'SpringBoot', link: '/pages/eaabb9/' },
-          { text: 'SpringCloud', link: '/pages/eaabb9/' },
-        ]
+      { text: 'spring全家桶', link: '/pages/5c591f/', 
       },
-      { text: '权限框架', link: '/pages/eaabb9/',
+      { text: '权限框架', link: '/pages/d8fc22/',
       items:[
-        { text: 'Shiro', link: '/pages/eaabb9/' },
-        { text: 'SpringSecurity', link: '/pages/eaabb9/' },
+        { text: 'Shiro', link: '/pages/2622ac/' },
+        { text: 'SpringSecurity', link: '/pages/d8fc22/' },
       ]
       
     },
-      { text: 'Mybatis', link: '/pages/4ee7f5/' },
-      { text: '中间件', link: '/pages/4ee7f5/'
+      { text: 'Mybatis', link: '/pages/79b4b1/' },
+      { text: '中间件', link: '/pages/f635df/'
       },
       
     ],
@@ -65,13 +59,12 @@ module.exports = [
     text: '其他',
     link: '/other/',
     items: [
-      { text: '设计模式', link: '/pages/e6c953/' },
-      { text: 'Git相关', link: '/pages/cfc81f/' },
-      { text: 'Docker', link: '/pages/829562/' },
+      { text: '设计模式', link: '/pages/b40d7a/' },
+      { text: 'Git相关', link: '/pages/bafb3a/' },
+      { text: 'Docker', link: '/pages/c259fd/' },
       { text: '智力题', link: '/pages/2d6562/' },
-      { text: '三高的理解', link: '/pages/56e2d2/' },
-      { text: '编码之道', link: '/pages/56e2d2/' },
-      { text: '安全之道', link: '/pages/e2e2c9/' }
+      { text: 'Linux', link: '/pages/84648d/' },
+      { text: '其他', link: '/pages/e2e2c9/' }
     ],
   }
 ]
